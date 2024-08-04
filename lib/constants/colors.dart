ebd64974374ab75fc8c63ddd92c1efbb9e85267b
rishigarg09 <rishigarg185@gmail.com>
2024-08-04 13:05:31 +0530

    changes in main, and adding constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryColor = Color(0xFFBBDEFB);
+  static const Color appbackgroundColor = Color(0xFFe0e0e0);
+  static const Color appbarColor =  Color(0xFFe0e0e0);
+  static const Color headingColor1 = Colors.black54;
+  static const Color textColor1= Color(0xFF989acd);
+  static const Color textColor2 = Color(0xFF878593);
+  static const Color bigTextColor=Color(0xFF2e2e31);
+  static const Color mainColor= Color(0xFF5d69b3);
+  static const Color starColor = Color(0xFFe7bb4e);
+  static const Color mainTextColor = Color(0xFFababad);
+
+
+  static const Color purple =  Color.fromRGBO(126, 58, 242, 1);
+  static const Color purpleBackground = Color.fromRGBO(241,231,255, 1);
+
+  // static final Color buttonBackground = Color(0xFFf1f1f9);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'package:ezy_click_app/screens/bottom_navigation_bar.dart';
-import 'package:ezy_click_app/screens/feature_pages/features_page.dart';
-import 'package:ezy_click_app/screens/home_page.dart';
-import 'package:ezy_click_app/screens/register_new_user.dart';
-import 'package:ezy_click_app/utils/routes.dart';
-import 'package:ezy_click_app/screens/add_product.dart';
-import 'package:ezy_click_app/screens/business_register.dart';
-import 'package:ezy_click_app/screens/otp_login_views/enter_otp.dart';
-import 'package:ezy_click_app/screens/otp_login_views/login_with_otp.dart';
-import 'package:ezy_click_app/screens/register_view.dart';
+import 'package:paygo_app/screens/bottom_navigation_bar.dart';
+import 'package:paygo_app/screens/home_page.dart';
+import 'package:paygo_app/screens/register_new_user.dart';
+import 'package:paygo_app/utils/routes.dart';
+import 'package:paygo_app/screens/add_product.dart';
+import 'package:paygo_app/screens/register_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   runApp(const AppInitializer());
@@ -24,42 +18,24 @@ class AppInitializer extends StatefulWidget {
 }
 
 class _AppInitializerState extends State<AppInitializer> {
-  bool onboardingCompleted = false;
-  bool hasValidSession = false;
 
   @override
   void initState() {
     super.initState();
-    initializeApp();
-  }
-
-  Future<void> initializeApp() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    onboardingCompleted = prefs.getBool('onboardingCompleted') ?? false;
-    hasValidSession = prefs.getBool('hasValidSession') ?? false;
-    FlutterNativeSplash.remove();
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Welcome to Ezy Click',
-      home: onboardingCompleted
-          ? (hasValidSession ? const Home() : const LoginMobileNumForm())
-          : const FeatureIllustrationApp(),
+      title: 'Welcome to PayGo',
+      home: const CustomBottomNavBar(),
       routes: {
-        loginPage: (context) => const LoginMobileNumForm(),
-        enterOtpView: (context) => const LoginOtpInpForm(),
         registerNewUser: (context) => RegisterUserPage(),
-        bottomNavigationBar: (context) => CustomBottomNavBar(),
+        bottomNavigationBar: (context) => const CustomBottomNavBar(),
         homeView: (context) => const Home(),
         registerView: (context) => const RegisterView(),
-        businessRegView: (context) => const BusinessRegistration(),
         addProduct: (context) => const AddProduct(),
-        featurePages: (context) => const FeatureIllustrationApp(),
       },
     );
   }
